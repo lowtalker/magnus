0842484de0855c17455928489d23c33d5226bbe5
Ted Przyzycki <tprzyzycki@gmail.com>
2016-12-16 09:16:05 -0500

    initial full-stack project

---
+++ src/main/java/me/scratch/magnus/Magnus.java
@@ -0,0 +1,16 @@
+package me.scratch.magnus;
+
+/**
+ *
+ * @author 
+ */
+public class Magnus {
+
+    /**
+     * @param args 
+     */
+    public static void main(String[] args) {
+            System.out.println("ruff");
+    }
+
+}
